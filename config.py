9b48405704bbf2f35820e4d4360a95efad6f5f71
Jeremy Sobczak <code@jerspace.com>
2025-03-19 01:16:09 -0400

    Remove config.py from tracking, now in .gitignore

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # take environment variables from .env.
-
-POSTGRES_MASTER_PWD = os.environ.get("POSTGRES_MASTER_PWD")
-DATABASE_URI = os.environ.get("DATABASE_URL")
-SECRET_KEY = os.environ.get("SECRET_KEY")
-IS_LOCAL = os.environ.get("IS_LOCAL", 0)
